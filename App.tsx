17bcbf868e28bc95b27c2efcfb4b7013345c1478
Diego Bogo <diego.bogo@biko2.com>
2023-07-26 08:13:52 +0200

    feat: add montserrat font variants

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from "react-native";
+import { Main } from "./src/components/Main";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    "mrt-regular": require("./assets/fonts/mrt-regular.ttf"),
+    "mrt-bold": require("./assets/fonts/mrt-bold.ttf"),
+    "mrt-extraBold": require("./assets/fonts/mrt-extraBold.ttf"),
+    "mrt-light": require("./assets/fonts/mrt-light.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <Text>Cargando...</Text>;
+  }
+
+  return <Main />;
+}
